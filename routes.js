e6f71452a9d3a0f3c2241614830d3b7f5f2ca68f
hectorleiva <hector@hectorleiva.com>
2015-04-30 16:28:14 -0400

    deleting the unneeded controller. Adding express static handling for the downloaded sitemaps.

--- routes.js
+++ routes.js
@@ -8,6 +8,11 @@ router.get('/', function(req, res) {
   res.send('Welcome to the home page');
 });
 
-router.get('/rendered_sitemaps', sitemaps.findRenderedSitemaps);
+router.get('/rendered_sitemaps', function(req, res) {
+  res.send('This is rendered_sitemaps directory. Within this directory there are defined sitemaps' +
+    ' that are divided up into their respected website domain names. Example: <br />' +
+    '\n <strong>http://spiderbites.nytimes.com/sitemaps/www.nytimes.com/sitemap.xml.gz</strong> <br />' +
+    '\n would have a directory created as: <strong>spiderbites.nytimes.com/</strong>');
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,9 +2,11 @@
 
 var express = require('express'),
     router = require('./routes'),
+    main_app = require('./app'),
     app = express();
 
 app.use(router);
+app.use('/rendered_sitemaps', express.static(__dirname + '/rendered_sitemaps'));
 
 module.exports.getServer = app.listen(3001, function() {
   console.log('Listening on port %d in %s mode', this.address().port, process.env.NODE_ENV);
